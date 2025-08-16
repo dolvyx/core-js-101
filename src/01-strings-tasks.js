ead9b41d3e95bc8f4dab0d27c605cd568c2ba6f0
dolvyx <anrusilkoton@gmail.com>
2025-08-17 02:07:07 +0300

    feat: add some tasks

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -33,7 +33,8 @@ function concatenateStrings(value1, value2) {
  *   'b'     => 1
  *   ''      => 0
  */
-function getStringLength(/* value */) {
+function getStringLength(value) {
+  return value.length;
   throw new Error('Not implemented');
 }
 
@@ -50,7 +51,8 @@ function getStringLength(/* value */) {
  *   'John','Doe'      => 'Hello, John Doe!'
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
+function getStringFromTemplate(firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}!`;
   throw new Error('Not implemented');
 }
 
@@ -64,7 +66,8 @@ function getStringFromTemplate(/* firstName, lastName */) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
+function extractNameFromTemplate(value) {
+
   throw new Error('Not implemented');
 }
 
@@ -79,7 +82,8 @@ function extractNameFromTemplate(/* value */) {
  *   'John Doe'  => 'J'
  *   'cat'       => 'c'
  */
-function getFirstChar(/* value */) {
+function getFirstChar(value) {
+  return value[0];
   throw new Error('Not implemented');
 }
 
